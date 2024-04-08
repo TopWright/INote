dbce895948e84c65050d7fd98577c32bdcf22b4c
Temitope Oluwaseyi <topwrightdev@gmail.com>
2024-04-08 13:55:48 +0100

    Update: Added API fetch hook

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class myDocument extends Document {
                 <Head>
                     <meta name="description" content="An Online Note Taking Apllication" />
                     <meta charSet="utf-8" />
-                    <meta name="robots" content="noindex, nofollow" />
+                    <meta name="robots" content="nofollow" />
                     <meta name="viewport" content="width=device-width" />
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
                 </Head >
